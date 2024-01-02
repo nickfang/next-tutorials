5f36fd7d673d7cceaf12695d5f9ae262d70862f2
Nick Fang <fang.nicholas@gmail.com>
2024-01-02 16:33:09 -0600

    Creating events-app tutorial site.  Created dummy routes for now.

---
+++ events-app/pages/events/[...slug].js
@@ -0,0 +1,12 @@
+import { useRouter } from 'next/router'
+
+function FilteredEventsPage() {
+  const router = useRouter();
+  return (
+    <div>
+      <h1>Filtered Events Page</h1>
+    </div>
+  )
+}
+
+export default FilteredEventsPage
---
+++ events-app/pages/events/[eventId].js
@@ -0,0 +1,13 @@
+import { useRouter } from 'next/router'
+
+function EventDetailPage() {
+  const router = useRouter();
+  const { eventId } = router.query;
+  return (
+    <div>
+      <h1>Event Detail Page</h1>
+    </div>
+  )
+}
+
+export default EventDetailPage
