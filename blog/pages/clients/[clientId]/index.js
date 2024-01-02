69c69bc5a7847b4d506bb8b47b050dcb0bd12174
Nick Fang <fang.nicholas@gmail.com>
2024-01-01 23:22:07 -0600

    Adding 404 page.

--- blog/pages/clients/[clientId]/index.js
+++ blog/pages/clients/[clientId]/index.js
@@ -1,7 +1,18 @@
+import { useRouter } from "next/router";
+
 function ClentProjectsPage() {
+  const router = useRouter();
+  function loadProjectHandler() {
+    // load data...
+    router.push({
+      pathname: '/clients/[clientId]/[clientprojectid]',
+      query: { clientId: 'max', clientprojectid: 'projecta' }
+    });
+  }
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   )
 }
